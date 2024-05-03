56af50aa451219835fed6c5361f982926a44ec7a
Q1unt <dima.kunitsien.03@gmail.com>
2024-05-03 20:22:44 +0300

    create webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "overrides": [
@@ -14,7 +15,12 @@ module.exports = {
             ],
             "parserOptions": {
                 "sourceType": "script"
-            }
+            },
+            
+            "files": ["**/*/*.test.js"],
+            "pludins": ["jest"],
+            "extends": ["plugin:jest/recommended"],
+            "rules": { "jest/prefer-expect-assertions":  "off" }
         }
     ],
     "parserOptions": {
